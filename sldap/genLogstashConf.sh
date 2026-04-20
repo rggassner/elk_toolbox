d574e6053aaac989132e4bfede3c6ba662a69975
rggassner <rafael.gassner@gmail.com>
2026-04-20 11:09:37 -0300

    Create genLogstashConf.sh

---
+++ sldap/genLogstashConf.sh
@@ -0,0 +1,17 @@
+cat genLogstashconf.sh 
+#!/bin/bash
+DIR="/etc/logstash/dictionaries/sldap"
+for file in `ls -1 output_yaml_files/`
+do
+        attribute=`basename $file .yml`
+        echo "                translate"
+        echo "                {"
+        echo "                        source => \"[user]\""
+        echo "                        target => \"[$attribute]\""
+        echo "                        dictionary_path => \"$DIR/$file\""
+        echo "                }"
+
+done
+mkdir -p $DIR
+cp output_yaml_files/* $DIR
+chown logstash: $DIR/*
